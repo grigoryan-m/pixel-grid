751c9568aeb5c79f20b5fb8b81ddbb67c25a4d41
grigoryan.m <waldm4rk@gmail.com>
2023-08-29 14:48:34 +0400

    Finished project

---
+++ main.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const gridSize = document.getElementById("grid-size");
+    const gridContainer = document.querySelector(".grid");
+    let currentColor = document.getElementById("color");
+
+    gridSize.addEventListener("input", ()=>{
+        gridContainer.innerHTML = "";
+        let size = gridSize.value;
+
+        gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+        gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
+        for(let i = 0; i < size; i++){
+            for(let k = 0; k < size; k++){
+                const gridItem = document.createElement("div");
+                gridItem.classList.add("grid-item");
+                gridItem.addEventListener("click", ()=>{
+                    gridItem.style.backgroundColor = currentColor.value;
+                });
+                gridItem.addEventListener("contextmenu", (event)=>{
+                    event.preventDefault();
+                    gridItem.style.backgroundColor = "transparent";
+                });
+                gridContainer.appendChild(gridItem);
+            }
+        }
+    });
+});
